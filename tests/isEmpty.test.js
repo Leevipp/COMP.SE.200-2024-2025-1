edb941a8d0ed3cc2dc1e453c52903582135a7703
vosku1 <venniosku@hotmail.fi>
2025-12-11 14:41:44 +0200

    Tests for isEmpty

--- tests/isEmpty.test.js
+++ tests/isEmpty.test.js
@@ -1,14 +1,42 @@
 import isEmpty from "../src/isEmpty.js";
 
-describe("isEmpty", () => {
+describe("tests for isEmpty", () => {
     test("should return true for null values", () => {
         expect(isEmpty(null)).toBeTruthy();
     });
 
+    test("should return true for empty array", () => {
+        expect(isEmpty([])).toBeTruthy();
+    });
 
+    test("should return true for empty map", () => {
+        expect(isEmpty(new Map())).toBeTruthy();
+    });
 
+    test("should return true for empty set", () => {
+        expect(isEmpty(new Set())).toBeTruthy();
+    });
 
+    test("should return true for empty object", () => {
+        expect(isEmpty({})).toBeTruthy();
+    });
 
+    test("should return true for boolean values", () => {
+        expect(isEmpty(true)).toBeTruthy();
+        expect(isEmpty(false)).toBeTruthy();
+    });
+
+    test("should return true for undefined", () => {
+        expect(isEmpty(undefined)).toBeTruthy();
+    });
+
+    test("should return false for object with values", () => {
+       expect(isEmpty({"a" : 1, "b" : 2})).toBeFalsy();
+    });
+
+    test("should return false for array with values", () => {
+        expect(isEmpty([1, 2, 3])).toBeFalsy();
+    });
 
 
 
